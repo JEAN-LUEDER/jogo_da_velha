1157b0205dfedda3898ed5aef6ba6deea2159265
JEAN-LUEDER <112573701+JEAN-LUEDER@users.noreply.github.com>
2023-05-20 19:21:52 -0300

    novas fuções

--- script.js
+++ script.js
@@ -1,78 +1,115 @@
-// Seleciona todas as células da tabela
-const cells = document.querySelectorAll("td");
+// Constantes do jogo -- parte logica do jogo--
+// combinaçoes possiveis vencedoras
+const PLAYER_1 = "X";
+const PLAYER_2 = "O";
+const WINNING_COMBINATIONS = [
+  ["00", "01", "02"], // Linhas horizontais
+  ["10", "11", "12"],
+  ["20", "21", "22"],
+  ["00", "10", "20"], // Linhas verticais
+  ["01", "11", "21"],
+  ["02", "12", "22"],
+  ["00", "11", "22"], // Linhas diagonais
+  ["02", "11", "20"]
+];
 
-// Inicia o jogo com o jogador X
-let currentPlayer = "X";
+// Variáveis do jogo
+let currentPlayer = PLAYER_1;
+let gameBoard = createGameBoard();
+let gameOver = false;
+let winningCells = null;
+let winningRow = null;
+let movesCouter = 0;
 
-// Adiciona um evento de clique a cada célula
-cells.forEach((cell) => {
-  cell.addEventListener("click", () => {
-    // Verifica se a célula está vazia
-    if (cell.textContent === "") {
-      // Marca a célula com o símbolo do jogador atual
-      cell.textContent = currentPlayer;
+// Função para criar o tabuleiro do jogo
+function createGameBoard() {
+  return [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
+}
 
-      // Verifica se o jogador atual ganhou o jogo
-      if (checkWin()) {
-        alert(`O jogador ${currentPlayer} ganhou!`);
+// Função para alternar o jogador atual
+function togglePlayer() {
+  currentPlayer = currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1;
+}
+
+// Função para processar o movimento do jogador
+function makeMove(row, col) {
+  if (!gameOver && gameBoard[row][col] === "") {
+    gameBoard[row][col] = currentPlayer;
+    document.getElementById("cell" + row + col).innerHTML = currentPlayer;
+    movesCouter++;
+
+    if (checkWinner()) {
+      gameOver = true;
+      setTimeout(function() {
+        alert("Jogador " + currentPlayer + " venceu!");
+      }, 100);
+      resetGame();
+    } else if (movesCouter === 9) {
+      gameOver = true;
+      setTimeout(function() {
+        alert("Empate!");
         resetGame();
-      } else if (checkTie()) { // Verifica se houve empate
-        alert("O jogo terminou em empate!");
-        resetGame();
-      } else {
-        // Alterna o jogador atual
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-      }
+      }, 100);
+    } else{
+      togglePlayer();
     }
+  }
+}
+
+
+// Função para verificar se há um vencedor
+function checkWinner() {
+  for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+    const [a, b, c] = WINNING_COMBINATIONS[i];
+    if (
+      gameBoard[a.charAt(0)][a.charAt(1)] === currentPlayer &&
+      gameBoard[b.charAt(0)][b.charAt(1)] === currentPlayer &&
+      gameBoard[c.charAt(0)][c.charAt(1)] === currentPlayer
+    ) {
+      winningRow = a.charAt(0);
+      return true;
+    }
+  }
+  return false;
+}
+
+// Função para reiniciar o jogo
+function resetGame() {
+  gameBoard = createGameBoard();
+  currentPlayer = PLAYER_1;
+  gameOver = false;
+  winningCells = null;
+  winningRow = null;
+  movesCouter = 0;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameBoard[i][j] = "";
+      document.getElementById("cell" + i + j).innerHTML = "";
+    }
+  }
+  
+  
+  if (winningRow !== null) {
+    document.getElementById(winningRow).classList.remove("winning-row");
+    winningRow = null;
+  }
+  gameOver = false;
+}
+
+// Eventos de clique para as células da tabela
+document.querySelectorAll("td").forEach(function(cell) {
+  cell.addEventListener("click", function() {
+    const row = cell.parentElement.rowIndex;
+    const col = cell.cellIndex;
+    makeMove(row, col);
   });
 });
 
-// Verifica se um jogador ganhou o jogo
-function checkWin() {
-  const winConditions = [
-    // Condições de vitória horizontal
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
+// Evento de clique para o botão de reinício
+document.getElementById("reset-button").addEventListener("click", resetGame);
 
-    // Condições de vitória vertical
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-
-    // Condições de vitória diagonal
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  // Verifica cada condição de vitória
-  for (let i = 0; i < winConditions.length; i++) {
-    const [a, b, c] = winConditions[i];
-    if (cells[a].textContent === currentPlayer &&
-        cells[b].textContent === currentPlayer &&
-        cells[c].textContent === currentPlayer) {
-      return true; // Um jogador ganhou
-    }
-  }
-
-  return false; // Nenhum jogador ganhou ainda
-}
-
-// Verifica se houve empate
-function checkTie() {
-  for (let i = 0; i < cells.length; i++) {
-    if (cells[i].textContent === "") {
-      return false; // Ainda existem células vazias
-    }
-  }
-
-  return true; // Todas as células estão preenchidas
-}
-
-// Reinicia o jogo
-function resetGame() {
-  cells.forEach((cell) => {
-    cell.textContent = "";
-  });
-  currentPlayer = "X";
-}
